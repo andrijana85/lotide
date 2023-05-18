7aeef18446722eb706cac9c65224cfdbe7268c10
andrijana85 <topalovic.andrijana@yahoo.com>
2023-05-18 11:20:31 -0400

    Adding assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3,4);
+assertEqual("Andrijana", "Andrijana");
