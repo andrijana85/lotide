6acfc8b2a1c593a40760df16fbc9d028b874e801
andrijana85 <topalovic.andrijana@yahoo.com>
2023-05-18 19:54:55 -0400

    Implement head

--- head.js
+++ head.js
@@ -7,9 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 
+const head = function(array) {
+  return array[0];
+  }
 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(3,4);
-assertEqual("Andrijana", "Andrijana");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
