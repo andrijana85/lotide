8080e5344e787505d51f01939c0e9446525df15b
andrijana85 <topalovic.andrijana@yahoo.com>
2023-05-24 14:29:37 -0400

    adding emoji

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -11,9 +11,9 @@ const eqArrays = function(array1, array2) {
 };
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed ${array1} === ${array2}`);
+    console.log(` ✅Assertion Passed ${array1} === ${array2}`);
   } else {
-    console.log(`Assertion Failed ${array1} !== ${array2}`);
+    console.log(` ❌Assertion Failed ${array1} !== ${array2}`);
   }
 };
 assertArraysEqual([1,2,3,4], [1,2,3,4]);
