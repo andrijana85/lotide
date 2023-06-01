4b8d675abcd09c20cc5f9a3dc88e08bedd797322
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-01 00:10:30 -0400

    add check if the values are equal

--- eqObjects.js
+++ eqObjects.js
@@ -21,20 +21,24 @@ const assertEqual = function(actual, expected) {
 
 
 const eqObjects = function(object1, object2) {
-  //create the arrays with an Objects keys using Object.keys
-  if(Object.keys(object1).length !== Object.keys(object2).length) {
+  //create the arrays with an Objects keys using Object.keys to compare the length
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    //if the length of arrays ia not equal return false
     return false;
-  }
+  }// console.log to check for bugs
+  // console.log("object has equal length");
   //loop with for..of since the keys are an array
-  for(let key of Object.keys(object1)) {
-    //check if they are arrays and pass them to eqArrays 
-  if(!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
-    return false;
-  } else if(!eqArrays(object1[key], object2[key])) {
-    return false;
+  for (const key of Object.keys(object1)) {
+    //check if they are arrays and pass them to eqArrays
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+    
+     if (!eqArrays(object1[key], object2[key])) {
+      return false;
+    } }else if (object1[key] !== object2[key]) { ////check if tha values are equal
+      return false;
     }
   }
-return true;
+  return true;
 
 };
 //test
