f6a02e7e449908d099f0b403191da5712ea86613
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-01 00:13:09 -0400

    modiffied assertObjectEqual.js with edited eqObject

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -4,17 +4,15 @@ const eqObjects = function(object1, object2) {
     //if the length of arrays ia not equal return false
     return false;
   }// console.log to check for bugs
-  console.log("object has equal length");
+  // console.log("object has equal length");
   //loop with for..of since the keys are an array
-  for (let key of Object.keys(object1)) {
+  for (const key of Object.keys(object1)) {
     //check if they are arrays and pass them to eqArrays
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      console.log("It is not array");
+    
+     if (!eqArrays(object1[key], object2[key])) {
       return false;
-    } else if (!eqArrays(object1[key], object2[key])) {
-      console.log("it is array");
-      return false;
-    } else if (object1[key] !== object2[key]) { ////check if tha values are equal
+    } }else if (object1[key] !== object2[key]) { ////check if tha values are equal
       return false;
     }
   }
