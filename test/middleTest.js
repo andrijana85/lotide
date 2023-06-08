afc9c486b593c409ddc8d048d5217e94468b3b58
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-08 10:15:26 -0400

    Test for middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-middle([1, 2, 3, 4, 5, 6, 7, 8]);
-middle([1, 2, 3, 7, 8]);
-middle([1]);
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// middle([1, 2, 3, 4, 5, 6, 7, 8]);
+// middle([1, 2, 3, 7, 8]);
+// middle([1]);
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), 2);
+  });
+
+  it("returns [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4,5]);
+  });
+  it("returns [] for [1,]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
