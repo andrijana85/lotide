6bc5f8ae60d21aa272e7e0c9d3228c8814fba546
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-07 23:40:45 -0400

    modified countLetters

--- countLetters.js
+++ countLetters.js
@@ -3,14 +3,13 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+  }};
 // declare an empty object obj to store the count of each letter.
 // Create a function called countLetters
 // The function uses for...of loop to iterate over each character letter in the string.
 // inside the for loop is If..else to check if the letter is ' '.
 // check if the property exists ( +1) or not (set to 0)
-//retun the obj
+// retun the obj
 
 const countLetters = function(string) {
   const obj = {};
