80ceda776eef309f54e99589eafcf2bbea0f00c7
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-07 23:35:17 -0400

    Modified function

--- test/headTest.js
+++ test/headTest.js
@@ -4,7 +4,7 @@
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 const assert = require('chai').assert;
-const head   = require('../head');
+const head = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
